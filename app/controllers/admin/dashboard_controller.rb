acd038ad4f6ad5afd82bc20a41abbef731fa78a9
Sian Stone <sian.m.stone@gmail.com>
2019-12-01 13:55:01 +0000

    basic admin styles for issue & media crud

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,8 @@
 class Admin::DashboardController < ApplicationController
   before_action :authenticate_user!
   def index
+    @issues = Issue.all.sort_by &:created_at
+    @medias = Media.all.sort_by &:created_at
+    # binding.pry
   end
 end
