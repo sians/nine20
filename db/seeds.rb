fee3e0d6996c26f870fa9edbce8cddea1d73f266
Sian Stone <sian.m.stone@gmail.com>
2019-12-08 14:37:33 +0000

    edits to image paths for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+laurie = User.new(email: "organic.almonds@gmail.com", password: "foobar", first_name: "Laurie", last_name: "Gerhardt")
+if laurie.save
+  puts "saved Laurie!"
+end
+
