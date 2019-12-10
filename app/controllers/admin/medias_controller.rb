ddfea415f46eb3cf20fb960a9e22ee56fd59ae28
Sian Stone <sian.m.stone@gmail.com>
2019-12-10 18:41:04 +0000

    removed accidental byebug

--- app/controllers/admin/medias_controller.rb
+++ app/controllers/admin/medias_controller.rb
@@ -27,7 +27,6 @@ class Admin::MediasController < ApplicationController
 
   def update
     if @media.update(media_params)
-      binding.pry
       redirect_to admin_medias_path
     else
       render 'admin/medias/edit'
