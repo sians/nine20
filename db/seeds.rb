863f34614cfa262bc2558c19902cb849db8cf609
Sian Stone <sian.m.stone@gmail.com>
2019-12-10 20:08:43 +0000

    trix editor added for media content, not yet implemented image upload

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,9 @@ if laurie.save
   puts "saved Laurie!"
 end
 
+
+
+jimbo = User.new(email: "james.benfield@googlemail.com", password: "password", first_name: "Jim", last_name: "Benfield")
+seb = User.new(email: "sdelamothe@gmail.com", password: "password", first_name: "Sebastian", last_name: "Delamothe")
+oli = User.new(email: "ocrunden@gmail.com", password: "password", first_name: "Oliver", last_name: "Crunden")
+jambo = User.new(email: "twomey.james218@gmail.com", password: "password", first_name: "James", last_name: "Twomey")
